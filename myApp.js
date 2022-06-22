b435a475380d04371342cf3b152f1bc1663bd48c
Anna Tyrka <anna.tyrka@sainsburys.co.uk>
2022-06-22 20:23:05 +0100

    add get method

--- myApp.js
+++ myApp.js
@@ -3,6 +3,12 @@ let app = express();
 
 console.log("Hello World")
 
+const handler = (req, res) => {
+  res.send('Hello Express')
+}
+app.get('/', (req, res) => {
+  res.send("Hello Express")
+})
 
 
 
